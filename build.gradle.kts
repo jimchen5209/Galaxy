8f6bc133b46dddadbaea8c05193384419269b8b3
jimchen5209 <jimchen5209@gmail.com>
2024-08-12 09:42:49 +0800

    build: update fabric api and mapping

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 val version = "0.0.1"
 val group = "one.oktw"
 
-val fabricVersion = "0.102.0+1.21.1"
+val fabricVersion = "0.102.1+1.21.1"
 val galaxyLibVersion = "305cdb36"
 
 repositories {
@@ -43,7 +43,7 @@ loom {
 dependencies {
     // Core
     minecraft(group = "com.mojang", name = "minecraft", version = "1.21.1")
-    mappings(group = "net.fabricmc", name = "yarn", version = "1.21.1+build.1", classifier = "v2")
+    mappings(group = "net.fabricmc", name = "yarn", version = "1.21.1+build.3", classifier = "v2")
     modImplementation(group = "net.fabricmc", name = "fabric-loader", version = "0.15.11")
 
     // fabric api
