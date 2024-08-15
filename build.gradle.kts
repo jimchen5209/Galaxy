35b2ba79b15b66cc2e77a3a8a7b2bd53dbc0be99
jimchen5209 <jimchen5209@gmail.com>
2024-08-15 14:37:35 +0800

    build: bump galaxy lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val version = "0.0.1"
 val group = "one.oktw"
 
 val fabricVersion = "0.102.1+1.21.1"
-val galaxyLibVersion = "305cdb36"
+val galaxyLibVersion = "7376fcdf"
 
 repositories {
     mavenCentral()
